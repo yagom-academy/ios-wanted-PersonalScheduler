3e4c4cef690cb5339a8252e46978cc412252c006
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-01-09 18:02:08 +0900

    VC파일 생성

---
+++ PersonalScheduler/InputSchedul/InputSchedulVC.swift
@@ -0,0 +1,16 @@
+//
+//  InputSchedulVC.swift
+//  PersonalScheduler
+//
+//  Created by 정재근 on 2023/01/09.
+//
+
+import UIKit
+
+class InputSchedulVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ PersonalScheduler/Login/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  PersonalScheduler
+//
+//  Created by 정재근 on 2023/01/09.
+//
+
+import UIKit
+
+class LoginVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
