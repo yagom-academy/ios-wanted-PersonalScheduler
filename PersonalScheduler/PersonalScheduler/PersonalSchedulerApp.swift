7596c6cbca70c6a18089c83c3bec4127ad9431aa
brad <gjrjs31@naver.com>
2023-01-09 11:14:33 +0900

    refactor: SwiftUI로 프로젝트 초기화

---
+++ PersonalScheduler/PersonalScheduler/PersonalSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalSchedulerApp.swift
+//  PersonalScheduler
+//
+//  Created by brad on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct PersonalSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
