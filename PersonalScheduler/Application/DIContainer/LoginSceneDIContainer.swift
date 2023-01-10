9b23ee200111cdbb59e38030245dacf1e6524f52
wongbingg <wb277@naver.com>
2023-01-10 22:40:15 +0900

    refactor : LoginSceneDIContainer 수정 #4
    - SigninViewController 생성 메서드 추가
    - SigninViewModel 생성 메서드 추가

--- PersonalScheduler/Application/DIContainer/LoginSceneDIContainer.swift
+++ PersonalScheduler/Application/DIContainer/LoginSceneDIContainer.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class LoginSceneDIContainer {
-    // MARK: Login
+    // MARK: - Login
     func makeLoginViewController(actions: LoginViewModelActions) -> LoginViewController {
         let viewModel = makeLoginViewModel(actions: actions)
         return LoginViewController(with: viewModel)
@@ -18,7 +18,17 @@ final class LoginSceneDIContainer {
         return DefaultLoginViewModel(actions: actions)
     }
     
-    // MARK: Login Flow Coordinator
+    // MARK: - Signin
+    func makeSigninViewController(actions: SigninViewModelActions) -> SigninViewController {
+        let viewModel = makeSigninViewModel(actions: actions)
+        return SigninViewController(with: viewModel)
+    }
+    
+    func makeSigninViewModel(actions: SigninViewModelActions) -> SigninViewModel {
+        return DefaultSigninViewModel(actions: actions)
+    }
+    
+    // MARK: - Login Flow Coordinator
     func makeLoginFlowCoordinator(navigationController: UINavigationController) -> LoginFlowCoordinator {
         return LoginFlowCoordinator(
             navigationController: navigationController,
