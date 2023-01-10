941e667517df4cd978ca3eae187144ffc5ddcc2a
Judy <joo00ee@naver.com>
2023-01-10 10:20:01 +0900

    feat: schduleTableview 생성 및 레이아웃 설정 #2

--- PersonalScheduler/ScheduleListViewController.swift
+++ PersonalScheduler/ScheduleListViewController.swift
@@ -8,9 +8,37 @@
 import UIKit
 
 class ScheduleListViewController: UIViewController {
+    private let schduleTableview: UITableView = {
+        let tableVeiw = UITableView(frame: .zero, style: .grouped)
+        tableVeiw.register(ScheduleListTableViewCell.self,
+                           forCellReuseIdentifier: ScheduleListTableViewCell.identifier)
+        tableVeiw.translatesAutoresizingMaskIntoConstraints = false
+        return tableVeiw
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        setupConstraint()
         view.backgroundColor = .systemBackground
+    }
+    
+    private func setupConstraint() {
+        view.addSubview(schduleTableview)
+        
+        NSLayoutConstraint.activate([
+            schduleTableview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                  constant: 8),
+            schduleTableview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                  constant: -8),
+            schduleTableview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                                  constant: 16),
+            schduleTableview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                                  constant: -16),
+        ])
     }
 }
 
