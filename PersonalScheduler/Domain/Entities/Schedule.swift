68d0ba1171ddd7fc6e39878a4c5c6e0da18d0d40
wongbingg <wb277@naver.com>
2023-01-10 22:32:22 +0900

    refactor : Schedule 모델 수정 #4
    - 임시값을 받아줄 ScheduleModel 생성
    - 복사값으로 변환하는 toCopy() 메서드 생성

--- PersonalScheduler/Domain/Entities/Schedule.swift
+++ PersonalScheduler/Domain/Entities/Schedule.swift
@@ -4,8 +4,28 @@
 //
 //  Created by 이원빈 on 2023/01/09.
 //
+import Foundation
 
-struct Schedule {
+struct Schedule: Equatable {
+    let id: UUID
+    let title: String
+    let body: String
+    let startDate: String
+    let endDate: String?
+    
+    func toCopy() -> ScheduleModel {
+        return ScheduleModel(
+            id: id,
+            title: title,
+            body: body,
+            startDate: startDate,
+            endDate: endDate
+        )
+    }
+}
+
+struct ScheduleModel {
+    let id: UUID
     let title: String
     let body: String
     let startDate: String
