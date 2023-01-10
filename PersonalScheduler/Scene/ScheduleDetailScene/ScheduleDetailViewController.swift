eb193b5b2bb2b73a6de4cac31fe94c279bcb8c09
Judy <joo00ee@naver.com>
2023-01-10 23:19:49 +0900

    feat: ScheduleDetailViewController 생성 #6

---
+++ PersonalScheduler/Scene/ScheduleDetailScene/ScheduleDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScheduleDetailViewController.swift
+//  PersonalScheduler
+//
+//  Created by Judy on 2023/01/10.
+//
+
+import UIKit
+
+final class ScheduleDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
