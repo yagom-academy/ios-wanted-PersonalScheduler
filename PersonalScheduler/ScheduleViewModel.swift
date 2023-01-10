4f2f0f64fffce050118b73aa840d133629b1a108
Judy <joo00ee@naver.com>
2023-01-10 15:49:56 +0900

    feat: ScheduleViewModel 구현 #2

---
+++ PersonalScheduler/ScheduleViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  ScheduleViewModel.swift
+//  PersonalScheduler
+//
+//  Created by Judy on 2023/01/10.
+//
+
+import Foundation
+
+protocol ScheduleViewModelInput {
+    func save(_ schedule: Schedule, at userID: String)
+    func fetch(at userID: String)
+    func delete(_ schedule: Schedule, at userID: String)
+}
+
+protocol ScheduleViewModelOutput {
+    var schedules: Observable<[Schedule]> { get set }
+    var error: Observable<String?> { get set }
+}
+
+protocol ScheduleViewModelType: ScheduleViewModelInput, ScheduleViewModelOutput { }
+
+final class ScheduleViewModel: ScheduleViewModelType {
+    private let scheduleFirestoreUseCase = ScheduleFirestoreUseCase()
+    
+    /// Output
+    var schedules: Observable<[Schedule]> = Observable([])
+    var error: Observable<String?> = Observable(nil)
+    
+    /// Input
+    func save(_ schedule: Schedule, at userID: String) {
+        scheduleFirestoreUseCase.save(schedule, at: userID) { result in
+            switch result {
+            case .success(_):
+                break
+            case .failure(let error):
+                self.error.value = error.localizedDescription
+            }
+        }
+    }
+    
+    func fetch(at userID: String) {
+        scheduleFirestoreUseCase.fetch(at: userID) { [weak self] result in
+            switch result {
+            case .success(let schedules):
+                self?.schedules.value = schedules
+            case .failure(let error):
+                self?.error.value = error.localizedDescription
+            }
+        }
+    }
+    
+    func delete(_ schedule: Schedule, at userID: String) {
+        scheduleFirestoreUseCase.delete(schedule, at: userID) { [weak self] result in
+            switch result {
+            case .success(_):
+                break
+            case .failure(let error):
+                self?.error.value = error.localizedDescription
+            }
+        }
+    }
+}
