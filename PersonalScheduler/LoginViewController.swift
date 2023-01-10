4d7535a5756b8fed6a320e663f0c4c73baf64a52
bard <schy2503@naver.com>
2023-01-10 22:04:15 +0900

    feat: 프로젝트 초기 설정
    - Main 스토리보드 파일 제거
    
    - info.plist 경로 설정
    
    - 폴더 설정

--- PersonalScheduler/LoginViewController.swift
+++ PersonalScheduler/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  PersonalScheduler
 //
 //  Created by kjs on 06/01/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
