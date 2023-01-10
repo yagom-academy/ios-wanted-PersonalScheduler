e0cd37ffd7add2a36a14bb4058ef31e4a222fbab
Ari <lee_ari95@icloud.com>
2023-01-10 19:26:50 +0900

    ✨ [feat] 로그인 버튼 구현

--- PersonalScheduler/Sources/Presentation/Auth/LoginButton.swift
+++ PersonalScheduler/Sources/Presentation/Auth/LoginButton.swift
@@ -5,4 +5,18 @@
 //  Created by Ari on 2023/01/10.
 //
 
-import Foundation
+import UIKit
+
+final class LoginButton: UIButton {
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 56, height: 56)
+    }
+
+    convenience init(image: UIImage?) {
+        self.init(frame: .zero)
+        let image = image?
+            .withRenderingMode(.alwaysOriginal)
+        setImage(image, for: .normal)
+    }
+}
