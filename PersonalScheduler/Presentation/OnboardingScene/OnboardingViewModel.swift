ac91df71f9dbdd1af634bec8347d489a0b4027cd
Neph <tony3322@naver.com>
2023-01-11 00:51:45 +0900

    [Feat] #1 온보딩 뷰모델 로그인 버튼 탭 기능 제작

--- PersonalScheduler/Presentation/OnboardingScene/OnboardingViewModel.swift
+++ PersonalScheduler/Presentation/OnboardingScene/OnboardingViewModel.swift
@@ -5,18 +5,44 @@
 //  Created by 천수현 on 2023/01/10.
 //
 
-import Foundation
+import UIKit
 
 final class OnboardingViewModel {
+
+    private let oAuthLoginUseCase = OAuthLoginUseCase()
+
+    var onboardingCardImages = [UIImage(systemName: "person"), UIImage(systemName: "star"), UIImage(systemName: "person")]
+
     func appleLoginButtonTapped() {
-        
+        oAuthLoginUseCase.execute(loginType: .apple) { result in
+            switch result {
+            case .success:
+                print("success")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
     func kakaoLoginButtonTapped() {
-        
+        oAuthLoginUseCase.execute(loginType: .kakao) { result in
+            switch result {
+            case .success:
+                print("success")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
     func naverLoginButtonTapped() {
-
+        oAuthLoginUseCase.execute(loginType: .apple) { result in
+            switch result {
+            case .success:
+                print("success")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
