3e0c3a1ab52005db5a4fea2fb10e2698ac0724cd
Judy <joo00ee@naver.com>
2023-01-10 23:21:34 +0900

    feat: ScheduleDatePickerView 생성 및 구현 #6

---
+++ PersonalScheduler/Scene/ScheduleDetailScene/ScheduleDatePickerView.swift
@@ -0,0 +1,56 @@
+//
+//  ScheduleDatePickerView.swift
+//  PersonalScheduler
+//
+//  Created by Judy on 2023/01/10.
+//
+
+import UIKit
+
+final class ScheduleDatePickerView: UIView {
+    private let entireStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.textColor = .label
+        label.adjustsFontForContentSizeCategory = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        return datePicker
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        entireStackView.addArrangedSubview(infoLabel)
+        entireStackView.addArrangedSubview(datePicker)
+        
+        self.addSubview(entireStackView)
+        
+        NSLayoutConstraint.activate([
+            entireStackView.topAnchor.constraint(equalTo: self.topAnchor),
+            entireStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            entireStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            entireStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
+}
