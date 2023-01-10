503151a5534377165f5c52a7e731affbc16f1f68
Neph <tony3322@naver.com>
2023-01-10 18:49:33 +0900

    [Feat] #1 로그인 버튼들 action 연결, 버튼 layout 조정

--- PersonalScheduler/Presentation/OnboardingScene/OnboardingViewController.swift
+++ PersonalScheduler/Presentation/OnboardingScene/OnboardingViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
-import AuthenticationServices
 
 final class OnboardingViewController: UIViewController {
 
     private let viewModel = OnboardingViewModel()
 
-    private lazy var appleLoginButton: ASAuthorizationAppleIDButton = {
-        let button = ASAuthorizationAppleIDButton(type: .signUp, style: .black)
+    private lazy var appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Apple로 로그인하기", for: .normal)
+        button.titleLabel?.textColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addAction(UIAction { [weak self] action in
             self?.viewModel.appleLoginButtonTapped()
@@ -23,6 +25,10 @@ final class OnboardingViewController: UIViewController {
 
     private lazy var kakaoLoginButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Kakao로 로그인하기", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.backgroundColor = .systemYellow
         button.addAction(UIAction { [weak self] action in
             self?.viewModel.kakaoLoginButtonTapped()
         }, for: .touchUpInside)
@@ -31,6 +37,10 @@ final class OnboardingViewController: UIViewController {
 
     private lazy var naverLoginButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Naver로 로그인하기", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.backgroundColor = .systemGreen
         button.addAction(UIAction { [weak self] action in
             self?.viewModel.naverLoginButtonTapped()
         }, for: .touchUpInside)
@@ -55,10 +65,12 @@ final class OnboardingViewController: UIViewController {
             kakaoLoginButton.bottomAnchor.constraint(equalTo: naverLoginButton.topAnchor, constant: -10),
             kakaoLoginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             kakaoLoginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            kakaoLoginButton.heightAnchor.constraint(equalToConstant: 50),
 
             appleLoginButton.bottomAnchor.constraint(equalTo: kakaoLoginButton.topAnchor, constant: -10),
             appleLoginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            appleLoginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+            appleLoginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            appleLoginButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
