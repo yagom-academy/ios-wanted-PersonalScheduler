b43d11804b730ccd2280f867f27704d8cd46d81e
minsson <mskaang@gmail.com>
2023-01-12 03:27:05 +0900

    [Feat] #1 카카오톡 로그인을 위해 AppDelegate, SceneDelegate 설정
    - https://developers.kakao.com/docs/latest/ko/kakaologin/ios#before-you-begin-setting-for-kakaotalk

--- PersonalScheduler/PersonalScheduler/AppDelegate.swift
+++ PersonalScheduler/PersonalScheduler/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import KakaoSDKCommon
+import KakaoSDKAuth
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +16,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
       
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        
+        return false
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        
+        return sceneConfiguration
     }
     
 }
--- PersonalScheduler/PersonalScheduler/SceneDelegate.swift
+++ PersonalScheduler/PersonalScheduler/SceneDelegate.swift
@@ -6,7 +6,16 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
     
 }
