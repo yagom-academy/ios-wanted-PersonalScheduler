4c8a3828e5d5048bf4339a72637fced29f9fb2f8
minsson <mskaang@gmail.com>
2023-01-11 15:35:05 +0900

    [Build] SwiftUI 기반 프로젝트 생성, gitignore 설정

---
+++ PersonalScheduler/PersonalScheduler/PersonalSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalSchedulerApp.swift
+//  PersonalScheduler
+//
+//  Created by minsson on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct PersonalSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
