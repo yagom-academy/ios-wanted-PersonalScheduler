6757c5b2421f9f82215b0354d231d666890cdd4d
minsson <mskaang@gmail.com>
2023-01-12 02:56:13 +0900

    [Feat] #1 SceneDelegate 파일 생성

---
+++ PersonalScheduler/PersonalScheduler/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  PersonalScheduler
+//
+//  Created by minsson on 2023/01/12.
+//
+
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+}
