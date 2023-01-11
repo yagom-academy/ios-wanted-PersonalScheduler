eda0bd2ae870a44b2b450002ad8c984f3ae92e72
Ari <lee_ari95@icloud.com>
2023-01-11 17:33:25 +0900

    ✨ [feat] 스케줄을 추가/편집 하는 화면에 TextField, DatePicker, TextView 등 뷰 요소를 추가

--- PersonalScheduler/Sources/Presentation/Schedule/ViewController/ScheduleViewController.swift
+++ PersonalScheduler/Sources/Presentation/Schedule/ViewController/ScheduleViewController.swift
@@ -38,6 +38,94 @@ class ScheduleViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.addSubviews(backgroundStackView)
+        NSLayoutConstraint.activate([
+            backgroundStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            backgroundStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            backgroundStackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            backgroundStackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            backgroundStackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor)
+        ])
+        return scrollView
+    }()
+    
+    private lazy var backgroundStackView = {
+        let stackView = UIStackView(axis: .vertical, alignment: .fill, distribution: .fill, spacing: 40.3)
+        stackView.backgroundColor = .clear
+        stackView.addArrangedSubviews()
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(
+            top: 20,
+            leading: 20,
+            bottom: 20,
+            trailing: 20
+        )
+        let titleView = ScheduleContentView(
+            icon: UIImage(systemName: "circle.fill")?.withTintColor(.psToday).withRenderingMode(.alwaysOriginal),
+            contentView: titleTextField
+        )
+        let startDateView = ScheduleContentView(
+            icon: UIImage(systemName: "clock")?.withTintColor(.label).withRenderingMode(.alwaysOriginal),
+            contentView: startDateSettingView
+        )
+        let endDateView = ScheduleContentView(
+            icon: nil,
+            contentView: endDateSettingView
+        )
+        let descriptionView = ScheduleContentView(
+            icon: UIImage(systemName: "highlighter")?.withTintColor(.label).withRenderingMode(.alwaysOriginal),
+            contentView: descriptionTextView
+        )
+        descriptionView.hideSeparator(true)
+        stackView.addArrangedSubviews(titleView, startDateView, endDateView, descriptionView)
+        return stackView
+    }()
+    
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.widthAnchor.constraint(equalToConstant: view.safeAreaLayoutGuide.layoutFrame.width - 76).isActive = true
+        textField.placeholder = "일정 제목"
+        textField.tintColor = .label
+        textField.font = .preferredFont(for: .body, weight: .regular)
+        return textField
+    }()
+    
+    private lazy var startDateSettingView: DateSettingView = {
+        let view = DateSettingView()
+        view.setUp(Date().nearestHour())
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(didTapStartDate(_:)))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    private lazy var endDateSettingView: DateSettingView = {
+        let view = DateSettingView()
+        view.setUp(Date().nearestHour().plusHour(1))
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(didTapEndDate(_:)))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    private let descriptionPlaceHolder = "일정 내용 작성하기 (500자 제한)"
+    
+    private lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.heightAnchor.constraint(equalToConstant: 420).isActive = true
+        textView.backgroundColor = .psBackground
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.textColor = .darkGray
+        textView.tintColor = .label
+        textView.text = descriptionPlaceHolder
+        textView.delegate = self
+        textView.contentInset = .zero
+        textView.textContainerInset = .zero
+        return textView
+    }()
+    
 }
 
 private extension ScheduleViewController {
@@ -49,6 +137,13 @@ private extension ScheduleViewController {
     
     func setUpLayout() {
         view.backgroundColor = .psBackground
+        view.addSubviews(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
  
     func setUpNavigationBar() {
@@ -64,12 +159,94 @@ private extension ScheduleViewController {
         navigationController?.addCustomBottomLine(color: .systemGray4, height: 0.3)
     }
     
-    @objc func didTapSaveButton(_ gesture: UIBarButtonItem) {
+    @objc func didTapSaveButton(_ sender: UIBarButtonItem) {
         print(#function)
+        
     }
     
-    @objc func didTapCancelButton(_ gesture: UIBarButtonItem) {
+    @objc func didTapCancelButton(_ sender: UIBarButtonItem) {
         coordinator?.dismiss()
     }
     
+    @objc func didTapStartDate(_ gesture: UITapGestureRecognizer) {
+        showDatePicker(Date().nearestHour())
+    }
+    
+    @objc func didTapEndDate(_ gesture: UITapGestureRecognizer) {
+        showDatePicker(Date().nearestHour().plusHour(1))
+    }
+    
+    func showDatePicker(_ date: Date) {
+        let datePicker = createDatePicker(date)
+        let dateChooserAlert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        dateChooserAlert.view.addSubviews(datePicker)
+        NSLayoutConstraint.activate([
+            datePicker.topAnchor.constraint(equalTo: dateChooserAlert.view.topAnchor),
+            datePicker.leadingAnchor.constraint(equalTo: dateChooserAlert.view.leadingAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: dateChooserAlert.view.trailingAnchor),
+            datePicker.bottomAnchor.constraint(equalTo: dateChooserAlert.view.bottomAnchor, constant: -60)
+        ])
+        dateChooserAlert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: { _ in
+            print(datePicker.date)
+        }))
+        present(dateChooserAlert, animated: true, completion: nil)
+    }
+    
+    func createDatePicker(_ date: Date) -> UIDatePicker {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ko_KR")
+        datePicker.minuteInterval = 5
+        datePicker.date = date
+        return datePicker
+    }
+}
+
+extension ScheduleViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == descriptionPlaceHolder {
+            textView.text = nil
+            textView.textColor = .label
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView.text, textView.text.count)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let newLength = textView.text.count - range.length + text.count
+        let koreanMaxCount = 500 + 1
+        if newLength > koreanMaxCount {
+            let overflow = newLength - koreanMaxCount
+            if text.count < overflow {
+                return true
+            }
+            let index = text.index(text.endIndex, offsetBy: Int(-overflow))
+            let newText = text[..<index]
+            guard let startPosition = textView.position(from: textView.beginningOfDocument, offset: range.location) else {
+                return false
+            }
+            guard let endPosition = textView.position(from: textView.beginningOfDocument, offset: NSMaxRange(range)) else {
+                return false
+            }
+            guard let textRange = textView.textRange(from: startPosition, to: endPosition) else {
+                return false
+            }
+            textView.replace(textRange, withText: String(newText))
+            return false
+        }
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = descriptionPlaceHolder
+            textView.textColor = .darkGray
+        }
+        if textView.text.count > 500 {
+            textView.text.removeLast()
+        }
+    }
 }
