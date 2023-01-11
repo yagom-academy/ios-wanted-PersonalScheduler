2969592aa4aac8671807d099c9be88aaa3052dd8
Judy <joo00ee@naver.com>
2023-01-11 22:52:14 +0900

    feat: UIViewController extension으로 얼럿 메서드 추가 #6

---
+++ PersonalScheduler/Utility/Extension/UIViewController+Alert.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Alert.swift
+//  PersonalScheduler
+//
+//  Created by Judy on 2023/01/11.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String = "", message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인",
+                                      style: .default))
+        self.present(alert, animated: true)
+    }
+}
