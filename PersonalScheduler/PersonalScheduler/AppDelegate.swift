3d53f77378ca5c445186eb3cf4a27f42caeeadc6
minsson <mskaang@gmail.com>
2023-01-12 03:02:37 +0900

    [Feat] #1 AppDelegate에 Kakao SDK를 초기화하는 코드 추가
    - https://developers.kakao.com/docs/latest/ko/getting-started/sdk-ios#init
    - SwiftUIApp을 위한 초기화 방법도 제공하지만 다른 외부 API들과의 통일성을 위해 AppDelegate 사용

--- PersonalScheduler/PersonalScheduler/AppDelegate.swift
+++ PersonalScheduler/PersonalScheduler/AppDelegate.swift
@@ -6,7 +6,15 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+      
+        return true
+    }
+    
 }
