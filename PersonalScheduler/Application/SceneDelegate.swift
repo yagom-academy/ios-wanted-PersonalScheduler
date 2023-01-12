0fc6d6cc7f6277ef6a8772053b525f6749bdd509
wongbingg <wb277@naver.com>
2023-01-12 12:32:54 +0900

    feat : SceneDelegate에 카카오 로그인서비스 적용 #4

--- PersonalScheduler/Application/SceneDelegate.swift
+++ PersonalScheduler/Application/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -27,6 +28,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window?.makeKeyAndVisible()
     }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                // MARK: Kakao Login Service
+                _ = AuthController.handleOpenUrl(url: url)
+            } else {
 
     func sceneDidDisconnect(_ scene: UIScene) {}
     func sceneDidBecomeActive(_ scene: UIScene) {}
