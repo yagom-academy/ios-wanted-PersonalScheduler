e6611200635d712ddcfbd5ded2c9cf32c535dfc7
wongbingg <wb277@naver.com>
2023-01-12 12:29:39 +0900

    feat : AppDelegate 에 카카오 sdk 초기화 #4

--- PersonalScheduler/Application/AppDelegate.swift
+++ PersonalScheduler/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        KakaoSDK.initSDK(appKey: "44bc39fa6aab979297a8e7e1b7a423be")
         return true
     }
 
