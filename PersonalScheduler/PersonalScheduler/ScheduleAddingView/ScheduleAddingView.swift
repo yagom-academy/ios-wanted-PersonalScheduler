2d23fdb5e5ea3f4e352daab53659ed61f666d7d6
minsson <mskaang@gmail.com>
2023-01-12 23:43:28 +0900

    [Feat] 스케줄 추가 뷰 내부 스케줄 인스턴스 기본값 지정

--- PersonalScheduler/PersonalScheduler/ScheduleAddingView/ScheduleAddingView.swift
+++ PersonalScheduler/PersonalScheduler/ScheduleAddingView/ScheduleAddingView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ScheduleAddingView: View {
+    
+    @Binding var shouldPresentAddingView: Bool
+    @State private var newSchedule: Schedule = Schedule(
+        id: UUID().uuidString,
+        title: "",
+        description: "",
+        startTime: Date(),
+        endTime: Date(),
+        status: .planned
+    )
+    
     var body: some View {
         Text("")
     }
 }
 
 struct ScheduleAddingView_Previews: PreviewProvider {
+    
+    @State static var shouldPresentSheet = false
+    
     static var previews: some View {
-        ScheduleAddingView()
+        ScheduleAddingView(shouldPresentAddingView: $shouldPresentSheet)
+    }
+    
+}
     }
 }
