58975615ea233a62565bac14e52bda487ab6e8fb
wongbingg <wb277@naver.com>
2023-01-12 12:53:38 +0900

    refactor : AlertBuilder 수정 #4
    - 프로토콜 채택 제거

--- PersonalScheduler/Utils/AlertBuilder.swift
+++ PersonalScheduler/Utils/AlertBuilder.swift
@@ -7,27 +7,21 @@
 
 import UIKit
 
-protocol AlertBuilder {
-    var alertController: UIAlertController { get }
-    
-    func setButton(name: String, style: UIAlertAction.Style, _ completion: (() -> Void)?) -> Self
-}
+final class AlertBuilder {
+    private var alertController: UIAlertController
 
-final class DefaultAlertBuilder: AlertBuilder {
-    var alertController: UIAlertController
-    
     init(title: String? = nil, message: String? = nil, preferredStyle: UIAlertController.Style) {
         alertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
     }
-    
-    func setButton(name: String, style: UIAlertAction.Style, _ completion: (() -> Void)? = nil) -> DefaultAlertBuilder {
+
+    func setButton(name: String, style: UIAlertAction.Style, _ completion: (() -> Void)? = nil) -> AlertBuilder {
         let button = UIAlertAction(title: name, style: style) { alertAction in
             completion?()
         }
         alertController.addAction(button)
         return self
     }
-    
+
     func showAlert(on viewController: UIViewController) {
         viewController.present(alertController, animated: true)
     }
