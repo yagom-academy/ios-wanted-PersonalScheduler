b14864617333d26f809a7bbe49d8c3ad2290c560
wongbingg <wb277@naver.com>
2023-01-12 12:52:40 +0900

    refactor : SigninViewModel 수정 #4
    - UseCase 추가

--- PersonalScheduler/Presentation/LoginScene/Signin/ViewModel/SigninViewModel.swift
+++ PersonalScheduler/Presentation/LoginScene/Signin/ViewModel/SigninViewModel.swift
@@ -6,23 +6,33 @@
 //
 
 struct SigninViewModelActions {
-    let registerButtonTapped: (LoginInfo) -> Void
+//    let successRegister: () -> Void
 }
 
 protocol SigninViewModelInput {
-    func registerButtonTapped(_ loginInfo: LoginInfo)
+    func registerButtonTapped(_ loginInfo: LoginInfo, _ completion: (String) -> Void) async throws
 }
 protocol SigninViewModelOutput {}
 protocol SigninViewModel: SigninViewModelInput, SigninViewModelOutput {}
 
 final class DefaultSigninViewModel: SigninViewModel {
     private let actions: SigninViewModelActions?
+    private let firebaseAuthUseCase: FirebaseAuthUseCase
     
-    init(actions: SigninViewModelActions? = nil) {
+    init(
+        actions: SigninViewModelActions? = nil,
+        firebaseAuthUseCase: FirebaseAuthUseCase
+    ) {
         self.actions = actions
+        self.firebaseAuthUseCase = firebaseAuthUseCase
     }
     
-    func registerButtonTapped(_ loginInfo: LoginInfo) {
-        actions?.registerButtonTapped(loginInfo)
+    func registerButtonTapped(_ loginInfo: LoginInfo, _ completion: (String) -> Void) async throws {
+        do {
+            let email = try await firebaseAuthUseCase.fetchUserEmail(from: loginInfo)
+            completion(email)
+        } catch let error {
+            print(error)
+        }
     }
 }
