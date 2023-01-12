828b0c9911dd2f26ce49f32b513546df858fd112
minsson <mskaang@gmail.com>
2023-01-13 04:07:34 +0900

    [Feat] String을 Date 타입으로 변환해주는 Date extension 구현

--- PersonalScheduler/PersonalScheduler/Models/ScheduleDTO.swift
+++ PersonalScheduler/PersonalScheduler/Models/ScheduleDTO.swift
@@ -5,6 +5,8 @@
 //  Created by minsson on 2023/01/12.
 //
 
+import Foundation
+
 struct ScheduleDTO {
     var id: String
     var title: String
@@ -12,4 +14,16 @@ struct ScheduleDTO {
     var startMoment: String
     var endMoment: String
     var status: String
+}
+
+fileprivate extension String {
+    func toDate() -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        guard let date = formatter.date(from: self) else {
+            return Date()
+        }
+        
+        return date
+    }
 }
