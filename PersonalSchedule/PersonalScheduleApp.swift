6bad9485c3aeb31b6f8a8d4b56183a922db34b5d
seohyeon2 <seo_hyeon418@naver.com>
2023-01-12 20:52:53 +0900

    feat: UIKit에서 SwiftUI로 변경

---
+++ PersonalSchedule/PersonalScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalScheduleApp.swift
+//  PersonalSchedule
+//
+//  Created by seohyeon park on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct PersonalScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
