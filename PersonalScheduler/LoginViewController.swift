083f8401405495f2027594f079fd504933d28c2c
SooJi <sokoooool86@gmail.com>
2023-01-12 20:59:09 +0900

    chore: 애플 로그인 버튼 capability 추가

--- PersonalScheduler/LoginViewController.swift
+++ PersonalScheduler/LoginViewController.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
