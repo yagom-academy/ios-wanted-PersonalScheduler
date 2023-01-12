50d0e654da44cb2d6ac354cc9335c8c8ac5d79cf
minsson <mskaang@gmail.com>
2023-01-12 22:02:32 +0900

    [Feat] #4 ScheduleListView 파일 생성

---
+++ PersonalScheduler/PersonalScheduler/ScheduleList/ScheduleListView.swift
@@ -0,0 +1,21 @@
+//
+//  ScheduleListView.swift
+//  PersonalScheduler
+//
+//  Created by minsson on 2023/01/12.
+//
+
+import SwiftUI
+
+struct ScheduleListView: View {
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ScheduleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleListView()
+    }
+}
