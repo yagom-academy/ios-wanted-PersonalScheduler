aa33cff6e6fbc6f240c529aced5a3a51b8346a7a
dnwhd0112 <dnwhd0112@naver.com>
2023-01-13 19:01:21 +0900

    refactor: 카카오 미설치시 웹으로도 로그인 할수 있게끔 수정

--- PersonalScheduler/Network/Login/KakaoLogin.swift
+++ PersonalScheduler/Network/Login/KakaoLogin.swift
@@ -14,24 +14,38 @@ final class KakaoLogin: Login {
     func getId() -> Observable<Result<String, Error>?> {
         let result: Observable<Result<String, Error>?> = .init(nil)
         
-        guard UserApi.isKakaoTalkLoginAvailable() == true else {
-            result.value = .failure(LoginError.kakaoInstallNeed)
-            return result
-        }
-        
-        UserApi.shared.loginWithKakaoTalk { token, error in
-            if error != nil {
-                result.value = .failure(LoginError.kakaoLodedError)
-            } else {
-                UserApi.shared.me { user, error in
-                    guard let id = user?.id else {
-                        result.value = .failure(LoginError.kakaoLodedError)
-                        return
+        if UserApi.isKakaoTalkLoginAvailable() == true {
+            UserApi.shared.loginWithKakaoTalk { token, error in
+                if error != nil {
+                    result.value = .failure(LoginError.kakaoLodedError)
+                } else {
+                    UserApi.shared.me { user, error in
+                        guard let id = user?.id else {
+                            result.value = .failure(LoginError.kakaoLodedError)
+                            return
+                        }
+                        result.value = .success(String(id))
                     }
-                    result.value = .success(String(id))
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount { token, error in
+                if error != nil {
+                    result.value = .failure(LoginError.kakaoLodedError)
+                } else {
+                    UserApi.shared.me { user, error in
+                        guard let id = user?.id else {
+                            result.value = .failure(LoginError.kakaoLodedError)
+                            return
+                        }
+                        result.value = .success(String(id))
+                    }
                 }
             }
         }
+        
+        
+        
          
         return result
     }
