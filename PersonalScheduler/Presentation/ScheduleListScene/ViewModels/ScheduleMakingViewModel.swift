d5c515a56c4a3ed46c72c325ba218cfa054b2ccf
Neph <tony3322@naver.com>
2023-01-13 18:54:57 +0900

    [Feat] 종료시간이 시작시간보다 앞서거나 같은 경우 alert 띄우기

--- PersonalScheduler/Presentation/ScheduleListScene/ViewModels/ScheduleMakingViewModel.swift
+++ PersonalScheduler/Presentation/ScheduleListScene/ViewModels/ScheduleMakingViewModel.swift
@@ -20,6 +20,12 @@ final class ScheduleMakingViewModel {
     func saveScheduleButtonTapped(title: String?, description: String?, startDate: Date, endDate: Date) {
         let schedule = Schedule(id: UUID(), title: title ?? "", description: description ?? "",
                                 startDate: startDate, endDate: endDate)
+        if startDate >= endDate {
+            let alert = UIAlertController(title: "종료시간은 시작시간 이후여야 합니다.", message: nil,
+                                          preferredStyle: .alert)
+            showAlert?(alert)
+            return
+        }
         saveScheduleUseCase.execute(schedule: schedule) { [weak self] result in
             switch result {
             case .success:
