87a92349c5efa7bc5bdd19a61b22050de9aa3b45
zhilly11 <zhzh1x4@gmail.com>
2023-02-07 01:05:15 +0900

    style: FileHeader 수정, 컨벤션에 따른 수정, 파일 Grouping

---
+++ PersonalScheduler/Source/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+//  PersonalScheduler - AppDelegate.swift
+//  Created by zhilly on 2023/02/07
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ PersonalScheduler/Source/View/Login/LoginViewController.swift
@@ -0,0 +1,11 @@
+//  PersonalScheduler - LoginViewController.swift
+//  Created by zhilly on 2023/02/07
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
