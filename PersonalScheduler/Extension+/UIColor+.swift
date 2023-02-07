65a14b4649aa9fa61530940310efd93f42d86393
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-02-07 14:45:27 +0900

    design: loginView 그리기- 달력이미지, 빈 뷰 추가 및 Layout

---
+++ PersonalScheduler/Extension+/UIColor+.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+.swift
+//  PersonalScheduler
+//
+//  Created by 써니쿠키 on 2023/02/07.
+//
+
+import UIKit
+
+extension UIColor {
+    class var primary: UIColor? { return UIColor(named: "Primary") }
+    class var secondary: UIColor? { return UIColor(named: "Secondary") }
+    class var tertiary: UIColor? { return UIColor(named: "Tertiary") }
+    class var navy: UIColor? { return UIColor(named: "Navy") }
+}
--- PersonalScheduler/View/LoginViewController.swift
+++ PersonalScheduler/View/LoginViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
+
+    private let calendarImageView: UIImageView = {
+        let calendarImage = UIImage(named: "Calendar")
+        let imageView = UIImageView(image: calendarImage)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    private let loginView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 30
+        view.backgroundColor = .tertiary
+
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawView()
     }
 
+    private func drawView() {
+        view.backgroundColor = .primary
+        configureHierarchy()
+        configureLayout()
+    }
 
+    private func configureHierarchy() {
+        view.addSubview(loginView)
+        view.addSubview(calendarImageView)
+    }
+
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            calendarImageView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            calendarImageView.centerYAnchor.constraint(equalTo: safeArea.topAnchor,
+                                                       constant: safeArea.layoutFrame.height/4),
+            calendarImageView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3),
+
+            loginView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            loginView.topAnchor.constraint(equalTo: calendarImageView.bottomAnchor, constant: 20),
+            loginView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+            loginView.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier:  0.8),
+        ])
+    }
 }
-
