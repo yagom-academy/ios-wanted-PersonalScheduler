f15cdbcf73639ce32f803086e76fea93d23400f1
Mangdi <lkw4670@naver.com>
2023-02-08 02:31:28 +0900

    chore: 타이틀 및 버튼 꾸미기

--- PersonalScheduler/Controller/ViewController.swift
+++ PersonalScheduler/Controller/ViewController.swift
@@ -12,24 +12,31 @@ class ViewController: UIViewController {
     // MARK: - Property
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "asdf"
+        label.text = "스케쥴케어"
         label.font = .preferredFont(forTextStyle: .title1)
         return label
     }()
 
-    private let kakaoLoginButton: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = 12
-        return button
+    private let kakaoLoginImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 12
+        imageView.image = UIImage(named: "kakao_login_medium_narrow")
+        imageView.contentMode = .scaleAspectFill
+//        imageView.sizeToFit()
+        return imageView
     }()
 
     private let facebookLoginButton: UIButton = {
         let button = UIButton()
+        button.layer.cornerRadius = 12
+        button.backgroundColor = UIColor.init(hex: "#5B7CF2")
         return button
     }()
 
     private let appleLoginButton: UIButton = {
         let button = UIButton()
+        button.layer.cornerRadius = 12
+        button.backgroundColor = UIColor.init(hex: "#000000")
         return button
     }()
 
@@ -45,7 +52,7 @@ class ViewController: UIViewController {
 private extension ViewController {
     func configureUI() {
         [titleLabel,
-         kakaoLoginButton,
+         kakaoLoginImageView,
          facebookLoginButton,
          appleLoginButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -56,21 +63,24 @@ private extension ViewController {
 
     func settingLayouts() {
         let smallSpacing: CGFloat = 20
+        let buttonHeight: CGFloat = 60
 
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 4),
 
-            kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            kakaoLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6),
-            kakaoLoginButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.frame.height / 4),
+            kakaoLoginImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            kakaoLoginImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6),
+            kakaoLoginImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.frame.height / 4),
 
             facebookLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             facebookLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6),
-            facebookLoginButton.topAnchor.constraint(equalTo: kakaoLoginButton.bottomAnchor, constant: smallSpacing),
+            facebookLoginButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            facebookLoginButton.topAnchor.constraint(equalTo: kakaoLoginImageView.bottomAnchor, constant: smallSpacing),
 
             appleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             appleLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6),
+            appleLoginButton.heightAnchor.constraint(equalToConstant: buttonHeight),
             appleLoginButton.topAnchor.constraint(equalTo: facebookLoginButton.bottomAnchor, constant: smallSpacing)
 
         ])
