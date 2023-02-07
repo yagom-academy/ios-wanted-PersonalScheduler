557e410312b60e5985887cc516b40dbcff853673
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-02-07 21:03:48 +0900

    build: KakaoOAuth 라이브러리 추가 (SPM)

--- PersonalScheduler/SupportingFiles/SceneDelegate.swift
+++ PersonalScheduler/SupportingFiles/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -20,6 +21,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = loginViewController
         window.makeKeyAndVisible()
         self.window = window
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
