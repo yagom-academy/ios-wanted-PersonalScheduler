00304f004e2959bf9eb2cc2d04ef17a338787ca1
Kyo <hyosing92@gmail.com>
2023-02-07 22:59:29 +0900

    feat : Scedule 타입 구현

---
+++ PersonalScheduler/Source/Model/Schedule.swift
@@ -0,0 +1,16 @@
+//
+//  Schedule.swift
+//  PersonalScheduler
+//
+//  Created by Kyo on 2023/02/07.
+//
+
+import Foundation
+
+struct Schedule {
+    let id: UUID
+    let startDate: Date
+    let endDate: Date
+    let title: String
+    let content: String
+}
