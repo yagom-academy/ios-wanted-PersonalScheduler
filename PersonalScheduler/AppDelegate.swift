cfc27c5c4fb328087c262ec5352c1d00399c5952
Mangdi <lkw4670@naver.com>
2023-02-07 18:43:20 +0900

    feat: 카카오sdk 초기화

--- PersonalScheduler/AppDelegate.swift
+++ PersonalScheduler/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "d9f7ce69b307d0099229d92f9633b9fe")
         return true
     }
 
