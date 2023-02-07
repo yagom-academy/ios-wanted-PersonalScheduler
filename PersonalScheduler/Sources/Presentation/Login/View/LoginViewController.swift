61118df74255a13958dbf482adbf9ec84f7ba28f
미니 <cow970814@naver.com>
2023-02-08 00:47:52 +0900

    :sparkles: Apple Login Method 생성

--- PersonalScheduler/Sources/Presentation/Login/View/LoginViewController.swift
+++ PersonalScheduler/Sources/Presentation/Login/View/LoginViewController.swift
@@ -6,11 +6,50 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
+    private let appleLoginButton: ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+        return button
+    }()
+    
+    private let viewModel = LoginViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(appleLoginButton)
+        appleLoginButton.center = self.view.center
+        
+        appleLoginButton.addTarget(self, action: #selector(tapAppleLogin), for: .touchUpInside)
+    }
+    
+    @objc func tapAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+        controller.performRequests()
     }
 }
 
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let user = credential.user
+            
+            print("user \(user)")
+            
+            if let email = credential.email {
+                print("email \(email)")
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Error \(error)")
+    }
+}
