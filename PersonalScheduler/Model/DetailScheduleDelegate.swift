2e0ccacffe3b9ba0d9fe42236b2f84231d4ef6f6
Mangdi <lkw4670@naver.com>
2023-02-09 01:48:38 +0900

    feat: DetailScheduleDelegate 생성

---
+++ PersonalScheduler/Model/DetailScheduleDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  DetailScheduleDelegate.swift
+//  PersonalScheduler
+//
+//  Created by Mangdi on 2023/02/09.
+//
+
+import Foundation
+
+protocol DetailScheduleDelegate: AnyObject {
+    func createSchedule(data: ScheduleModel)
+    func updateSchedule(date: ScheduleModel)
+}
