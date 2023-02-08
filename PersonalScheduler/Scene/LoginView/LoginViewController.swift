b03733703bd787d9efa30579123571c2e872f132
wonbi92 <bin9239@gmail.com>
2023-02-08 17:03:53 +0900

    feat: Facebook로그인 버튼 구현

--- PersonalScheduler/Scene/LoginView/LoginViewController.swift
+++ PersonalScheduler/Scene/LoginView/LoginViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+import FacebookLogin
 
+class LoginViewController: UIViewController {
+    private let facebookLoginButton: FBLoginButton = {
+        let button = FBLoginButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .brown
+        view.backgroundColor = .systemBackground
+        configureView()
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            facebookLoginButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            facebookLoginButton.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor)
+        ])
+    }
+    
+    private func configureView() {
+        view.addSubview(facebookLoginButton)
     }
 }
