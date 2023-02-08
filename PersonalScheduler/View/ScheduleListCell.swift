ec9cb95921da43b9a349265e230c7710df27bb89
Mangdi <lkw4670@naver.com>
2023-02-08 18:27:56 +0900

    feat: 셀 레이아웃 구성

--- PersonalScheduler/View/ScheduleListCell.swift
+++ PersonalScheduler/View/ScheduleListCell.swift
@@ -30,11 +30,39 @@ final class ScheduleListCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-        self.layer.borderWidth = 2
-        self.layer.borderColor = UIColor(hex: "#04CC00").cgColor
+        configureUI()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension ScheduleListCell {
+    func configureUI() {
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor(hex: "#04CC00").cgColor
+
+        [titleLabel, bodyLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            addSubview($0)
+        }
+
+        settingLayouts()
+    }
+
+    func settingLayouts() {
+        let smallSpacing: CGFloat = 20
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: smallSpacing),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: smallSpacing),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: smallSpacing),
+
+            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: smallSpacing),
+            bodyLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: smallSpacing),
+            bodyLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: smallSpacing),
+            bodyLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: smallSpacing)
+        ])
     }
 }
