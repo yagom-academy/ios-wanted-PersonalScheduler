2809638fa4fb14bdeda6cdab9f832f044d541f77
wonbi92 <bin9239@gmail.com>
2023-02-08 17:03:27 +0900

    feat: Facebook 로그인을 위한 SDK 설치

--- PersonalScheduler/AppDelegate.swift
+++ PersonalScheduler/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+import FacebookCore
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
     }
 
