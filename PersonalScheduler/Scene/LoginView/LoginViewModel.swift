4080b2b6f366d9c95aa604183a0c5b0c7ce1d5a6
wonbi92 <bin9239@gmail.com>
2023-02-09 02:10:19 +0900

    feat: LoginViewModel 프로퍼티 구현

--- PersonalScheduler/Scene/LoginView/LoginViewModel.swift
+++ PersonalScheduler/Scene/LoginView/LoginViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import FacebookLogin
+
+
+final class LoginViewModel {
+    enum Action {
+        case tapKakaoLogin
+        case tapFacebookLogin
+    }
+    
+    private let facebookLoginManager = LoginManager()
+    private let service: LoginService
+    
+    init(service: LoginService) {
+        self.service = service
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .tapKakaoLogin:
+            break
+        case .tapFacebookLogin:
+            break
+        }
+    }
+}
