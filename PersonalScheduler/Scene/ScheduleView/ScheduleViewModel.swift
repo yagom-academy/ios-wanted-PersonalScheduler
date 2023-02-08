af9dfbf8c019371f99144eb3503c691a210c68db
wonbi92 <bin9239@gmail.com>
2023-02-08 15:02:22 +0900

    feat: ScheduleViewModel Action 및 기본 프로퍼티 구현

--- PersonalScheduler/Scene/ScheduleView/ScheduleViewModel.swift
+++ PersonalScheduler/Scene/ScheduleView/ScheduleViewModel.swift
@@ -12,3 +12,22 @@ protocol ScheduleViewModelDelegate: AnyObject {
     func scheduleViewModel(selectedScheduleID id: String)
     func scheduleViewModel(failedFetchData error: RemoteDBError)
 }
+
+final class ScheduleViewModel {
+    enum Action {
+        case viewWillAppear
+        case tapSchedule(indexPath: IndexPath)
+    }
+    
+    private let userID: String
+    private let service: ScheduleServiceable
+    private weak var delegate: ScheduleViewModelDelegate?
+    
+    private var schedules: [Schedule] = [] {
+    }
+    
+    init(userID: String, service: ScheduleServiceable) {
+        self.userID = userID
+        self.service = service
+    }
+}
