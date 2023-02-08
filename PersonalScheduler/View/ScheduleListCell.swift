76081d2c821fbbedd7af06d87b61162ee6d3ba82
Mangdi <lkw4670@naver.com>
2023-02-08 18:21:57 +0900

    feat: 셀에 들어갈 UI요소 생성

--- PersonalScheduler/View/ScheduleListCell.swift
+++ PersonalScheduler/View/ScheduleListCell.swift
@@ -12,9 +12,26 @@ final class ScheduleListCell: UITableViewCell {
     // MARK: - Property
     static let identifier = String(describing: ScheduleListViewController.self)
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        return label
+    }()
+
+    private let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .systemGray3
+        label.numberOfLines = 3
+        return label
+    }()
+
     // MARK: - LifeCycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor(hex: "#04CC00").cgColor
     }
 
     required init?(coder: NSCoder) {
