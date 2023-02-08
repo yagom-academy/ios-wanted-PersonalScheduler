ba7fa46debc9160352c7ce878e6afe29ef069c08
미니 <cow970814@naver.com>
2023-02-08 21:25:46 +0900

    :recycle: Kakao Login Code 기능별 함수 분리

---
+++ PersonalScheduler/Sources/Domain/Services/FirebaseAuthService.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseAuthService.swift
+//  PersonalScheduler
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import FirebaseAuth
+
+class FirebaseAuthService: LoginService {
+    var isSuccess: PassthroughSubject<Bool, Never>
+    
+    func login() {
+        <#code#>
+    }
+}
