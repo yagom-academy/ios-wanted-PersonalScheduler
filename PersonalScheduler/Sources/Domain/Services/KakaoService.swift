f8da23c0950c91c0112b412de50e86c4725a2d0d
미니 <cow970814@naver.com>
2023-02-08 15:51:01 +0900

    :sparkles: Kakao Login Service 타입 구현

--- PersonalScheduler/Sources/Domain/Services/KakaoService.swift
+++ PersonalScheduler/Sources/Domain/Services/KakaoService.swift
@@ -3,6 +3,80 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
+import Combine
 import Foundation
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+
+final class KakaoLoginService: LoginService {
+    @Published var isSuccess = false
+    func authorization() -> AnyPublisher<Bool, Never> {
+        login()
+        return AnyPublisher($isSuccess)
+    }
+    
+    func login() {
+        if AuthApi.hasToken() {
+            UserApi.shared.accessTokenInfo { _, error in
+                guard error == nil else {
+                    self.handleTokenError(error: error)
+                    return
+                }
+                self.loginKakao()
+            }
+        } else {
+            self.loginKakao()
+        }
+    }
+    
+    func handleTokenError(error: Error?) {
+        guard let error = error as? SdkError else {
+            isSuccess = false
+            return
+        }
+        
+        if error.isInvalidTokenError() {
+            loginKakao()
+        }
+    }
+    
+    func loginKakao() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk { token, error in
+                guard error == nil else {
+                    self.isSuccess = false
+                    return
+                }
+                self.isSuccess = true
+            }
+        }
+        
+        if UserApi.isKakaoTalkLoginAvailable() == false {
+            UserApi.shared.loginWithKakaoAccount { token, error in
+                guard error == nil else {
+                    self.isSuccess = false
+                    return
+                }
+                self.isSuccess = true
+            }
+        }
+    }
+    
+    func readUserInformation() {
+        UserApi.shared.me { user, error in
+            if let error = error {
+                self.isSuccess = false
+            } else {
+                guard let email = user?.kakaoAccount?.email,
+                      let id = user?.id else {
+                    self.isSuccess = false
+                    return
+                }
+                
+                self.isSuccess = true
+            }
+        }
+    }
+}
