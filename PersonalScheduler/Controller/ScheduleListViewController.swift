e617e5bc1bafa45974c05bbd2739bd4124aa2a70
Mangdi <lkw4670@naver.com>
2023-02-08 16:19:12 +0900

    feat: tableView 추가 및 레이아웃제약걸어주기

--- PersonalScheduler/Controller/ScheduleListViewController.swift
+++ PersonalScheduler/Controller/ScheduleListViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 
 class ScheduleListViewController: UIViewController {
 
+    // MARK: - Property
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-        settingNavigationBar()
+        configureUI()
     }
 }
 
 // MARK: - UIConfiguration
 private extension ScheduleListViewController {
+    func configureUI() {
+        view.addSubview(tableView)
+        settingNavigationBar()
+        settingLayouts()
+    }
+
+    func settingLayouts() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+
     func settingNavigationBar() {
         navigationItem.title = "스케쥴케어"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
