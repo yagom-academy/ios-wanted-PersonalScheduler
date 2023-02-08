09f993a4f337cb88136b3b3c302cbacc1c501d14
junho <junhoL1046@gmail.com>
2023-02-08 23:43:04 +0900

    feat: Facebook 로그인 기능 구현

--- PersonalScheduler/Controllers/LoginViewController.swift
+++ PersonalScheduler/Controllers/LoginViewController.swift
@@ -6,10 +6,92 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FacebookLogin
 
 final class LoginViewController: UIViewController {
+    private var handle: AuthStateDidChangeListenerHandle?
+    private var isMFAEnabled = false
+    private let stackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+        return stackView
+    }()
+    private let facebookLoginButton = {
+        let button = FBLoginButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureHierarchy()
+        configureLoginButton()
+    }
+
+    private func configureHierarchy() {
+        stackView.addArrangedSubview(facebookLoginButton)
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+
+    private func configureLoginButton() {
+        facebookLoginButton.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handle = Auth.auth().addStateDidChangeListener { auth, user in
+          // ...
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        guard let handle else { return }
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
+}
+
+extension LoginViewController: LoginButtonDelegate {
+    func loginButton(
+        _ loginButton: FBSDKLoginKit.FBLoginButton,
+        didCompleteWith result: FBSDKLoginKit.LoginManagerLoginResult?,
+        error: Error?) {
+            if let error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let tokenString = AccessToken.current?.tokenString else { return }
+            let credential = FacebookAuthProvider.credential(withAccessToken: tokenString)
+            firebaseLogin(credential)
+    }
+
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginKit.FBLoginButton) {
+        do {
+            try Auth.auth().signOut()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension LoginViewController {
+    func firebaseLogin(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { authResult, error in
+            if let error {
+                let authError = error as NSError
+                print(authError.localizedDescription)
+            }
+        }
     }
 }
