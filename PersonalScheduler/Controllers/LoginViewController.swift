96be9cbe009a237af3e8bbe43c69d7cb74090847
junho <junhoL1046@gmail.com>
2023-02-08 21:04:51 +0900

    feat: Main.storyboard 제거 및 LoginViewController 추가

---
+++ PersonalScheduler/Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  PersonalScheduler
+//
+//  Created by junho lee on 2023/02/08.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
