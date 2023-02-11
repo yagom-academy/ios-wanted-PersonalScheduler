85b13c3a854569e14e6504a33e3b64481ee23986
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-02-11 21:09:48 +0900

    docs: 소스코드 문서화

--- PersonalScheduler/Source/View/Scheduler/SchedulerViewController.swift
+++ PersonalScheduler/Source/View/Scheduler/SchedulerViewController.swift
@@ -48,7 +48,8 @@ final class SchedulerViewController: BaseViewController {
         return dataSource
     }()
     
-    // MARK: - Life Cycle
+    
+    // MARK: - Initialize
     
     init(viewModel: SchedulerViewModel, userName: String) {
         self.viewModel = viewModel
@@ -60,6 +61,8 @@ final class SchedulerViewController: BaseViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
