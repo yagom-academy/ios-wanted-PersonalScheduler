21ac0350d812d868f3cbf16b5158ff3bdb76eb37
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-02-11 19:36:02 +0900

    feat: 키보드 나타나고 사라질 때 화면처리 - 스크롤 뷰에 아이템을 담은 후 contentInset 으로 핸들링

--- PersonalScheduler/View/RegisterViewController.swift
+++ PersonalScheduler/View/RegisterViewController.swift
@@ -22,10 +22,16 @@ final class RegisterViewController: UIViewController {
 
     var delegate: RegisterViewControllerDelegate?
     private let viewModel: RegisterViewModel
-    private let viewLabel = UILabel(font: .title2,
-                                    fontBold: true,
-                                    textColor: .navy,
-                                    textAlignment: .center)
+    private let viewLabel = UILabel(font: .title2, fontBold: true, textColor: .navy, textAlignment: .center)
+    private let titleField = UITextField(font: .title2, radius: 12)
+    private let datePicker = UIDatePicker(mode: .date, style: .wheels)
+    private let startLabel = UILabel(font: .title3, textColor: .black, textAlignment: .right)
+    private let endLabel = UILabel(font: .title3, textColor: .black, textAlignment: .right)
+    private let startTimePicker = UIDatePicker(mode: .time, style: .compact)
+    private let endTimePicker = UIDatePicker(mode: .time, style: .compact)
+    private let startTimeStackView = UIStackView(spacing: 10, margin: 10)
+    private let endTimeStackView = UIStackView(spacing: 10, margin: 10)
+    private let totalStackView = UIStackView(axis: .vertical, distribution: .fill, spacing: 10, margin: 10)
 
     private let descriptionTextView: UITextView = {
         let textView = UITextView(frame: .zero)
@@ -50,27 +56,28 @@ final class RegisterViewController: UIViewController {
 
         return button
     }()
-    
-    private let titleField = UITextField(font: .title2, radius: 12)
-    private let datePicker = UIDatePicker(mode: .date, style: .wheels)
-    private let startLabel = UILabel(font: .title3, textColor: .black, textAlignment: .right)
-    private let endLabel = UILabel(font: .title3, textColor: .black, textAlignment: .right)
-    private let startTimePicker = UIDatePicker(mode: .time, style: .compact)
-    private let endTimePicker = UIDatePicker(mode: .time, style: .compact)
-    private let startTimeStackView = UIStackView(spacing: 10, margin: 10)
-    private let endTimeStackView = UIStackView(spacing: 10, margin: 10)
-    private let totalStackView = UIStackView(axis: .vertical, distribution: .fill, spacing: 10, margin: 10)
+
+    private let scrollView: UIScrollView = {
+        let scrollview = UIScrollView()
+        scrollview.keyboardDismissMode = .interactive
+        scrollview.translatesAutoresizingMaskIntoConstraints = false
+
+        return scrollview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         configureHierarchy()
         configureLayout()
+        bindViewModel()
+        addKeyboardNotifications()
     }
 
     init(viewModel: RegisterViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
-        bindViewModel()
+
     }
 
     required init?(coder: NSCoder) {
@@ -101,6 +108,38 @@ final class RegisterViewController: UIViewController {
     }
 }
 
+//MARK: - Handling Keyboard
+extension RegisterViewController {
+
+    private func addKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(setKeyboardShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(setKeyboardHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+
+    @objc private func setKeyboardShow(_ notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
+                as? NSValue else { return }
+
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+
+        self.scrollView.contentInset.bottom = keyboardHeight
+
+        if self.descriptionTextView.isFirstResponder {
+            self.scrollView.contentOffset.y = keyboardHeight
+        }
+    }
+
+    @objc private func setKeyboardHide(_ notification: Notification) {
+        self.scrollView.contentInset.bottom = 0
+    }
+}
+
 //MARK: - ViewHierarchy and Layout
 extension RegisterViewController {
 
@@ -113,11 +152,13 @@ extension RegisterViewController {
             endTimeStackView.addArrangedSubview(view)
         }
 
-        [viewLabel, titleField, datePicker, startTimeStackView, endTimeStackView, descriptionTextView].forEach { view in
+        [titleField, datePicker, startTimeStackView, endTimeStackView, descriptionTextView].forEach { view in
             totalStackView.addArrangedSubview(view)
         }
 
-        view.addSubview(totalStackView)
+        scrollView.addSubview(totalStackView)
+        view.addSubview(scrollView)
+        view.addSubview(viewLabel)
         view.addSubview(registerButton)
     }
 
@@ -126,21 +167,32 @@ extension RegisterViewController {
         descriptionTextView.setContentHuggingPriority(.defaultLow, for: .vertical)
 
         NSLayoutConstraint.activate([
+            viewLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            viewLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            viewLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            viewLabel.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1),
+
             registerButton.centerYAnchor.constraint(equalTo: viewLabel.centerYAnchor),
             registerButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             registerButton.heightAnchor.constraint(equalTo: viewLabel.heightAnchor, multiplier: 0.5),
             registerButton.widthAnchor.constraint(equalTo: registerButton.heightAnchor),
 
-            viewLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
-
             titleField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
 
             endLabel.widthAnchor.constraint(equalTo: startLabel.widthAnchor),
 
-            totalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            totalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            totalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            totalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            descriptionTextView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.4),
+
+            totalStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            totalStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            totalStackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            totalStackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+
+            scrollView.contentLayoutGuide.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: viewLabel.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
