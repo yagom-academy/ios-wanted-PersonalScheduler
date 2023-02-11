29f4fc77e9b06e4396db4e65ab14988993e30c1f
미니 <cow970814@naver.com>
2023-02-12 00:52:17 +0900

    :lipstick: DetailViewController 구성

--- PersonalScheduler/Sources/Presentation/ScheduleDetail/View/ScheduleDetailViewController.swift
+++ PersonalScheduler/Sources/Presentation/ScheduleDetail/View/ScheduleDetailViewController.swift
@@ -3,6 +3,62 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+
+final class ScheduleDetailViewController: UIViewController {
+    private let titleTextField: ScheduleTextField = {
+        let textField = ScheduleTextField()
+        textField.placeholder = "할일의 제목을 입력해주세요."
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+}
+
+private extension ScheduleDetailViewController {
+    func configureUI() {
+        addSafeArea()
+        addChildComponents()
+        setUpLayout()
+    }
+    
+    func addSafeArea() {
+        additionalSafeAreaInsets.top += 16
+        additionalSafeAreaInsets.left += 16
+        additionalSafeAreaInsets.bottom += 16
+        additionalSafeAreaInsets.right += 16
+    }
+    
+    func addChildComponents() {
+        [
+            titleTextField
+        ].forEach(view.addSubview)
+    }
+    
+    func setUpLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            titleTextField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            titleTextField.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            titleTextField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct ScheduleDetailViewPreview: PreviewProvider {
+    static var previews: some View {
+        ScheduleDetailViewController()
+            .showPreview()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+#endif
--- PersonalScheduler/Sources/Presentation/ScheduleDetail/View/Subviews/ScheduleTextField.swift
+++ PersonalScheduler/Sources/Presentation/ScheduleDetail/View/Subviews/ScheduleTextField.swift
@@ -3,6 +3,44 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+
+final class ScheduleTextField: UITextField {
+    private var padding: UIEdgeInsets {
+        get {
+            return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        }
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        layer.borderColor = UIColor(named: "textFieldBorderColor")?.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 8.5
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowRadius = 3
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowOpacity = 1.0
+        layer.masksToBounds = false
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
