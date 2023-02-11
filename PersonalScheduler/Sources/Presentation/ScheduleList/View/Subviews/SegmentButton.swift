67cedb03d8d27e2c2dcb15720d807938e506bea5
미니 <cow970814@naver.com>
2023-02-11 22:58:18 +0900

    :lipstick: SegmentButton 타입 구현

--- PersonalScheduler/Sources/Presentation/ScheduleList/View/Subviews/SegmentButton.swift
+++ PersonalScheduler/Sources/Presentation/ScheduleList/View/Subviews/SegmentButton.swift
@@ -3,6 +3,32 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+
+final class SegmentButton: UIButton {
+    override var isSelected: Bool {
+        didSet {
+            changeButtonState()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+        
+        setTitleColor(.white, for: .selected)
+    }
+    
+    private func changeButtonState() {
+        UIView.animate(withDuration: 0.5, delay: 0) { [weak self] in
+            guard let isSelected = self?.isSelected else { return }
+            
+            if isSelected {
+                self?.backgroundColor = UIColor(named: "segementSelectedColor")
+            } else {
+                self?.backgroundColor = .white
+            }
+        }
+    }
+}
