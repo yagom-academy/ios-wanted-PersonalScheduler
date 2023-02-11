b73ba18aa995930d6c8b4c690432f73df77744b1
미니 <cow970814@naver.com>
2023-02-12 00:53:35 +0900

    :recycle: NavigationBar View 타입 구성

--- PersonalScheduler/Sources/Presentation/Utility/NavigationView.swift
+++ PersonalScheduler/Sources/Presentation/Utility/NavigationView.swift
@@ -3,6 +3,16 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+
+class NavigationBar: UIView {
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = UIColor(named: "skyBlue")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
