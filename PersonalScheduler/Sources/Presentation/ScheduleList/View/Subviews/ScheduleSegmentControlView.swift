b04ba40901893cd9116dd39b502e2ccbdaec4b73
미니 <cow970814@naver.com>
2023-02-11 22:59:06 +0900

    :lipstick: SegmentControl 커스텀 타입 구현

--- PersonalScheduler/Sources/Presentation/ScheduleList/View/Subviews/ScheduleSegmentControlView.swift
+++ PersonalScheduler/Sources/Presentation/ScheduleList/View/Subviews/ScheduleSegmentControlView.swift
@@ -3,6 +3,106 @@
 //  PersonalScheduler
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+import Combine
+
+final class ScheduleSegmentControlView: UIView {
+    private var itemTitles: [String] = []
+    
+    private var buttons: [UIButton] = []
+    private var stackView = UIStackView()
+    private var selectedColor: UIColor = .white
+    
+    private var cancellable = Set<AnyCancellable>()
+    
+    convenience init(frame: CGRect, titles: [String]) {
+        self.init(frame: frame)
+        self.itemTitles = titles
+        
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        updateView()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = bounds.height / 2
+        backgroundColor = .white
+    }
+    
+    private func updateView() {
+        createButtons()
+        configureStackView()
+        
+        bindAction()
+    }
+    
+    private func createButtons() {
+        buttons.removeAll()
+        subviews.forEach { $0.removeFromSuperview() }
+        
+        for title in itemTitles {
+            let button = SegmentButton()
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.backgroundColor = .clear
+            button.titleLabel?.textColor = .black
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            buttons.append(button)
+        }
+        
+        let textColor = UIColor(named: "titleAccentColor")
+        
+        buttons[0].setTitleColor(textColor, for: .normal)
+        buttons[0].isSelected = true
+    }
+    
+    private func configureStackView() {
+        stackView = UIStackView(arrangedSubviews: buttons)
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 5
+        stackView.distribution = .fillEqually
+        stackView.layoutMargins = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.backgroundColor = .clear
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    private func bindAction() {
+        buttons.forEach { button in
+            button.tapPublisher
+                .sink { [weak self] _ in
+                    self?.changeButtonsState(button)
+                }
+                .store(in: &cancellable)
+        }
+    }
+    
+    private func changeButtonsState(_ sender: UIButton) {
+        buttons.forEach { $0.isSelected = false }
+        sender.isSelected = true
+    }
+}
