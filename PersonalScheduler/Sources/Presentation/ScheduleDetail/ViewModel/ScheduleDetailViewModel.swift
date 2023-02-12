2f6e0fdad96a75789a378faf9673d698dc03844a
미니 <cow970814@naver.com>
2023-02-12 11:48:40 +0900

    :recycle: Detail ViewModel 구현

--- PersonalScheduler/Sources/Presentation/ScheduleDetail/ViewModel/ScheduleDetailViewModel.swift
+++ PersonalScheduler/Sources/Presentation/ScheduleDetail/ViewModel/ScheduleDetailViewModel.swift
@@ -7,5 +7,16 @@
 import Combine
 
 final class ScheduleDetailViewModel {
+    private let service = FireStoreService<[Schedule]>(referenceName: "Users")
+    private var cancellable = Set<AnyCancellable>()
     // TODO: - Schedule 저장, 삭제, 업데이트
+    
+    func readData() {
+        service.readData(documentReference: "Schedule", collectionReference: "Example")
+            .replaceError(with: nil)
+            .sink { values in
+                print(values)
+            }
+            .store(in: &cancellable)
+    }
 }
