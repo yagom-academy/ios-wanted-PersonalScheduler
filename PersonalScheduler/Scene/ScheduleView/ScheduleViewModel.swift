d8e4b13bee915958b76f19395f9308b92a932659
wonbi92 <bin9239@gmail.com>
2023-02-12 09:32:27 +0900

    feat: ScheduleViewModel 구현

--- PersonalScheduler/Scene/ScheduleView/ScheduleViewModel.swift
+++ PersonalScheduler/Scene/ScheduleView/ScheduleViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol ScheduleViewModelDelegate: AnyObject {
-    func scheduleViewModel(didChange schedules: [Schedule])
+    func scheduleViewModel(didChange startDateWithSchedules: [String: [SchedulePreview]])
     func scheduleViewModel(selectedScheduleID id: String)
     func scheduleViewModel(failedFetchData error: RemoteDBError)
 }
@@ -16,16 +16,18 @@ protocol ScheduleViewModelDelegate: AnyObject {
 final class ScheduleViewModel {
     enum Action {
         case viewWillAppear
-        case tapSchedule(indexPath: IndexPath)
+        case tapSchedule(section: String, index: Int)
+        case findSectionTitle(section: Int, completion: ((String) -> Void))
     }
     
+    private let dispatchGroup = DispatchGroup()
     private let userID: String
     private let service: ScheduleServiceable
-    private weak var delegate: ScheduleViewModelDelegate?
+    weak var delegate: ScheduleViewModelDelegate?
     
-    private var schedules: [Schedule] = [] {
+    private var periodWithSchedules: [String: [SchedulePreview]] = [:] {
         didSet {
-            delegate?.scheduleViewModel(didChange: schedules)
+            delegate?.scheduleViewModel(didChange: periodWithSchedules)
         }
     }
     
@@ -38,8 +40,10 @@ final class ScheduleViewModel {
         switch action {
         case .viewWillAppear:
             setInitialSchedules()
-        case .tapSchedule(let indexPath):
-            tapScheduleCell(indexPath: indexPath)
+        case .tapSchedule(let section, let index):
+            tapScheduleCell(section: section, index: index)
+        case .findSectionTitle(let section, let completion):
+            findSectionTitle(from: section, completion: completion)
         }
     }
     
@@ -47,16 +51,30 @@ final class ScheduleViewModel {
         service.requestAllSchedule(from: userID) { [weak self] result in
             switch result {
             case .success(let schedules):
-                self?.schedules = schedules
+                var result: [String: [SchedulePreview]] = [:]
+                let schedulePreviews = schedules .map { $0.convertForViewModel() }
+                let keys = schedulePreviews.map { $0.startDateString }.uniqued()
+                
+                keys.forEach { key in
+                    result[key] = schedulePreviews.filter { $0.startDateString == key }
+                }
+                
+                self?.periodWithSchedules = result
             case .failure(let error):
                 self?.delegate?.scheduleViewModel(failedFetchData: error)
             }
         }
     }
     
-    private func tapScheduleCell(indexPath: IndexPath) {
-        let selectedSchedule = schedules[indexPath.row]
+    private func tapScheduleCell(section: String, index: Int) {
+        guard let selectedSchedule = periodWithSchedules[section]?[index] else { return }
         
         delegate?.scheduleViewModel(selectedScheduleID: selectedSchedule.id)
+    }
+    
+    func findSectionTitle(from section: Int, completion: ((String) -> Void)) {
+        let keys = periodWithSchedules.keys.sorted(by: >)
+        
+        completion(keys[section])
     }
 }
